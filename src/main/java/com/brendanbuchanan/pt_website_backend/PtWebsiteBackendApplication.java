e113f2e2d426c2274e5152aef98de91867824857
Brendan Buchanan <brendanbuchanan21@gmail.com>
2025-06-07 15:45:15 -0500

    spiderman

---
+++ src/main/java/com/brendanbuchanan/pt_website_backend/PtWebsiteBackendApplication.java
@@ -0,0 +1,13 @@
+package com.brendanbuchanan.pt_website_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PtWebsiteBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PtWebsiteBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/brendanbuchanan/pt_website_backend/PtWebsiteBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.brendanbuchanan.pt_website_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PtWebsiteBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
